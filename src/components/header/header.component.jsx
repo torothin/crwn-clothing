39ea152f16e5e375711f8ba6d433f322956d5aea
Brad Duvall <bradford.duvall@gmail.com>
2019-10-03 14:55:04 -0700

    updating paths and need to find a better fix

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -13,21 +13,21 @@ import './header.styles.scss';
 
 const Header = ({ currentUser, hidden }) => (
     <div className='header'>
-        <Link className='logo-container' to='/'>
+        <Link className='logo-container' to='/crwn-clothing/'>
             <Logo className='logo' />
         </Link>
         <div className='options'>
-            <Link className='option' to='/shop'>
+            <Link className='option' to='/crwn-clothing/shop'>
                 SHOP
             </Link>
-            <Link className='option' to='/shop'>
+            <Link className='option' to='/crwn-clothing/shop'>
                 CONTACT
             </Link>
             {
                 currentUser ?
                 <div className='option' onClick={() => auth.signOut()}>SIGN OUT</div>
                 :
-                <Link className='option' to='/signin'>SIGN IN</Link>
+                <Link className='option' to='/crwn-clothing/signin'>SIGN IN</Link>
             }
             <CartIcon />
         </div>

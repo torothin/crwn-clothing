2e75106482e1d562594d62b76eaaa8824b520500
Brad Duvall <bradford.duvall@gmail.com>
2019-10-03 14:04:53 -0700

    Updates

---
+++ precache-manifest.67af97b6f32dae88e58e3b99eab55978.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6c6c405dbd1a1ca0086983bcaaaabc0",
+    "url": "/crwn-clothing/index.html"
+  },
+  {
+    "revision": "9cb19dd6e66409546d71",
+    "url": "/crwn-clothing/static/css/main.965b13d9.chunk.css"
+  },
+  {
+    "revision": "a54d5c9c0370583be9a5",
+    "url": "/crwn-clothing/static/js/2.a7f083b4.chunk.js"
+  },
+  {
+    "revision": "9cb19dd6e66409546d71",
+    "url": "/crwn-clothing/static/js/main.734bdf73.chunk.js"
+  },
+  {
+    "revision": "9a78e82df8f8529e1571",
+    "url": "/crwn-clothing/static/js/runtime~main.7afac7a2.js"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/crwn-clothing/static/media/crown.ed28accb.svg"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/crwn-clothing/static/media/shopping-bag.30f07071.svg"
+  }
+]);
